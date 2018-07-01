590d27fb9ac406105add4ecf8638978f806a75dc
wayne1204 <b04501095@ntu.edu.tw>
2018-07-01 16:30:35 +0800

    front end

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from server2 import Server
 import sys
 
 if __name__ == '__main__':
-    HOST, PORT = "172.20.10.4", int(sys.argv[1])
+    HOST, PORT = "", int(sys.argv[1])
     ser = Server(HOST, PORT)
     ser.run()
